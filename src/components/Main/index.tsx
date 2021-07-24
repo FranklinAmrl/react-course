5b82f46c5e5920f4f92742cf3493bb0229809cd8
Franklin Amaral <franklinamaralt@gmail.com>
2021-07-24 19:11:27 -0300

    Creating the first styles

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,12 @@
 /* eslint-disable prettier/prettier */
+import * as S from './styles'
+
 const Main = () => 
-    <main>
-        <h1>React Avançado</h1>
-    </main>
+    <S.Wrapper>
+        <S.Logo src="/img/logo.svg" alt="Imagem de um átomo e React Avançado escrito ao lado." />
+        <S.Title>React Avançado</S.Title>
+        <S.Description>TypeSCropt, ReactJS, NextJS e Styled Components.</S.Description>
+        <S.Illustration src="/img/hero-illustration.svg" alt="Imagem de um boneco sentado de costas mexendo no computador"/>
+    </S.Wrapper>
 
 export default Main

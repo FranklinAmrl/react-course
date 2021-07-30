02a51f90cb0ca314face7d53c30583cd52c69be5
Franklin Amaral <franklinamaralt@gmail.com>
2021-07-29 22:53:28 -0300

    Fixing the jest.config and build-storybook

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
-/* eslint-disable prettier/prettier */
+// eslint-disable-next-line no-undef
 module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/storybook.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
